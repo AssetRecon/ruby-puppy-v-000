6546e87d88d1a0865d5c69ba0833425135d261e2
Edsel Caprice <caprice@mail.usf.edu>
2018-06-28 01:55:25 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,16 +4,20 @@ attr_reader :name
 
 def initialize(name)
 @name = name
+@@all << self
 end
+
+
 def all
   @@all.each do |i|
     puts i
   end
 end
 
-
-
+def self.clear_all
+@@all = []
 end
+
 
 
 
